74beb83d654ca9aa1f3de4d5f8568a67cf88a200
Sofiananthya <145470302+Sofiananthya@users.noreply.github.com>
2023-10-16 07:36:50 -0600

    Add files via upload

---
+++ PIZZA/src/main/java/nsgl/pizza/PIZZA.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package nsgl.pizza;
+
+/**
+ *
+ * @author User
+ */
+public class PIZZA {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
